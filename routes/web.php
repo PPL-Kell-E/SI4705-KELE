0ff83e312445439ff2323b3ada0a24d63bb3011c
Cindyafrizoelia <cindyafri6@gmail.com>
2026-05-04 16:18:44 +0700

    PKE-2

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\HealthDataController;
-
-Route::get('/', [HealthDataController::class, 'index'])->name('health.index');
-Route::post('/health-data', [HealthDataController::class, 'store'])->name('health.store');
-Route::put('/health-data/{id}', [HealthDataController::class, 'update'])->name('health.update');
